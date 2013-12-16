26dfd42f735916186f8ed8e3b38402aed7a0fd92
Tom Lazar <tomster@pyfidelity.com>
2013-12-17 00:11:03 +0100

    configure the middleware so that submissions are sent to the editors

--- deployment/freebsd/roles/appserver/files/import-gpg.sh
+++ deployment/freebsd/roles/appserver/files/import-gpg.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-env GNUPGHOME=/usr/local/briefkasten/var/pgp_pubkeys gpg --import /usr/local/briefkasten/var/pgp_pubkeys/*@*
+env GNUPGHOME=/usr/local/briefkasten/var/pgp_pubkeys gpg --import /usr/local/briefkasten/var/pgp_pubkeys/*.gpg
