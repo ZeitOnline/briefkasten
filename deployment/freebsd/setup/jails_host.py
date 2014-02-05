f365fc34f730f2d786c1516a504a4f5b3b078c4f
Tom Lazar <tomster@pyfidelity.com>
2014-02-05 14:21:23 +0100

    move building of ports from ansible to fabric

--- deployment/freebsd/setup/jails_host.py
+++ deployment/freebsd/setup/jails_host.py
@@ -1,15 +1,19 @@
 # coding: utf-8
 from fabric import api as fab
 from mr.awsome.ezjail.fabric import bootstrap as _bootstrap
-from fabric_scripts import _local_path, _rsync_project
+from fabric_scripts import _local_path, _rsync_project, _default_vars
 
 fab.env.shell = '/bin/sh -c'
+default_vars = _default_vars('poudriere.yml')
 
 
 def bootstrap(**kwargs):
+    """call mr.awsome.ezjail's bootstrap """
     with fab.lcd(_local_path('provisioning/vm-master')):
         _bootstrap(**kwargs)
 
+
+# poudriere tasks:
 
 def download_distfiles():
     _rsync_project(remote_dir='/usr/local/poudriere/distfiles',
@@ -32,3 +36,10 @@ def download_ports_tree():
 
 def upload_distfiles():
     pass
+
+
+def build_packages():
+    """ uploads the list of packages and tells poudriere to build them"""
+    pkg_list = default_vars['pkg_list']
+    fab.put(_local_path('setup/roles/poudriere/files/pkg_list'), pkg_list)
+    fab.run('poudriere bulk -f %s -j 92amd64' % pkg_list)
