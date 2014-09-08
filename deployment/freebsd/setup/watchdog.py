e41be7081fee77401aac74815cc8718bcb57ef66
Tom Lazar <tomster@pyfidelity.com>
2014-09-08 16:48:51 +0200

    create the watchdog configuration file from ploy.conf

--- deployment/freebsd/setup/watchdog.py
+++ deployment/freebsd/setup/watchdog.py
@@ -1,10 +1,9 @@
 # coding: utf-8
-from datetime import datetime
 from os import path
 from fabric import api as fab
-from fabric.api import env, task
+from fabric.contrib.files import upload_template
+from fabric.api import env
 from ploy.config import value_asbool
-from ploy.common import yesno
 from bsdploy.fabutils import rsync_project
 
 env.shell = '/bin/sh -c'
@@ -16,20 +15,35 @@ ansible_vars = None
 from fabric.state import output
 output['stdout'] = False
 
+defaults = {
+    'watchdog_max_process_secs': 60,
+}
+
 
 def get_vars():
     global ansible_vars
     if ansible_vars is None:
         print "fetching ansible vars"
         hostname = env.host_string.split('@')[-1]
-        ansible_vars = dict(hostname=hostname, **env.instances[hostname].get_ansible_variables())
+        ansible_vars = dict(hostname=hostname, **defaults)
+        ansible_vars.update(**env.instances[hostname].get_ansible_variables())
 
 
 def install_watchdog(build=True):
     get_vars()
     app_dir = ansible_vars['watchdog_app_dir']
     app_user = ansible_vars['watchdog_app_user']
+
     with fab.lcd('../../../watchdog'):
+        upload_template(
+            filename='watchdog.ini.in',
+            template_dir=path.abspath(path.join(path.dirname(__file__), '../../../watchdog')),
+            destination='{watchdog_app_dir}/watchdog.ini'.format(**ansible_vars),
+            context=ansible_vars,
+            use_jinja=True,
+            mode='444',
+        )
+
         if value_asbool(build):
             fab.local('rm -rf dist')
             fab.local('../deployment/freebsd/bin/mkrelease -n')
