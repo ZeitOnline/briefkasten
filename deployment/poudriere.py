bc267d22fd00e3227e3a9e1b5825039ba0a0a865
Tom Lazar <tomster@pyfidelity.com>
2015-10-27 09:52:29 +0100

    clean up poudriere setup
    and use a publicly available repo soecifically for deploying the
    briefkasten dependencies

--- deployment/poudriere.py
+++ deployment/poudriere.py
@@ -5,56 +5,26 @@ from bsdploy.fabutils import rsync
 fab.env.shell = '/bin/sh -c'
 
 
+@fab.task
 def download_distfiles():
-    _rsync_project(remote_dir='/usr/local/poudriere/distfiles',
-        local_dir=_local_path('downloads/'),
-        delete=False, upload=False)
+    rsync('-av', '{host_string}:/usr/local/poudriere/distfiles', 'downloads/')
 
 
+@fab.task
 def upload_distfiles():
-    _rsync_project(remote_dir='/usr/local/poudriere/distfiles/',
-        local_dir=_local_path('downloads/distfiles/'),
-        delete=True, upload=True)
+    rsync('-av', 'downloads/distfiles/', '{host_string}:/usr/local/poudriere/distfiles/')
 
 
+@fab.task
 def download_packages():
-    _rsync_project(remote_dir='/usr/jails/basejail/poudriere_data/packages',
-        local_dir=_local_path('downloads/'),
-        delete=False, upload=False)
+    rsync('-av', '{host_string}:/usr/jails/basejail/poudriere_data/packages',
+        'downloads/')
 
 
+@fab.task
 def upload_packages():
-    _rsync_project(remote_dir='/usr/jails/basejail/poudriere_data/packages/',
-        local_dir=_local_path('downloads/packages/'),
-        delete=False, upload=True)
-
-
-def download_ports_tree():
-	""" download poudriere's ports tree """
-	_rsync_project(remote_dir='/usr/local/poudriere/ports/default',
-        local_dir=_local_path('downloads/'),
-        delete=False, upload=False)
-
-
-def upload_ports_tree():
-	""" download poudriere's ports tree """
-	_rsync_project(remote_dir='/usr/local/poudriere/ports/default/',
-        local_dir=_local_path('downloads/default/'),
-        delete=False, upload=True)
-
-
-def download_poudriere_assets():
-    """ download ports tree, distfiles, and packages from poudriere """
-    download_distfiles()
-    download_packages()
-    download_ports_tree()
-
-
-def upload_poudriere_assets():
-    """ upload local ports tree, distfiles, and packages from poudriere """
-    upload_distfiles()
-    upload_packages()
-    upload_ports_tree()
+    rsync('-av', '{host_string}:/usr/jails/basejail/poudriere_data/packages/',
+        'downloads/packages/')
 
 
 @fab.task
@@ -65,3 +35,19 @@ def download_options():
 @fab.task
 def build_packages():
     fab.run('poudriere bulk -f /usr/local/etc/poudriere.d/briefkasten-pkglist -p briefkasten -z briefkasten -j 102amd64')
+
+
+@fab.task
+def download_poudriere_assets():
+    """ download ports tree, distfiles, and packages from poudriere """
+    download_distfiles()
+    download_packages()
+
+
+@fab.task
+def upload_poudriere_assets():
+    """ upload local ports tree, distfiles, and packages from poudriere """
+    upload_distfiles()
+    upload_packages()
+
+
