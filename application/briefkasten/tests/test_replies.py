ef0e252db01829b0b06bba8909730cbe83b7465e
Tom Lazar <tomster@pyfidelity.com>
2016-01-08 13:31:22 +0100

    fix test by avoiding hard-coded url

--- application/briefkasten/tests/test_replies.py
+++ application/briefkasten/tests/test_replies.py
@@ -1,17 +1,13 @@
 # -*- coding: utf-8 -*-
-from briefkasten import dropbox_container
-
-
 def test_non_existent_drop_id_raises_not_found(browser):
     response = browser.get('/briefkasten/foobar', status=404)
     assert response.status == '404 Not Found'
 
 
-def test_submitter_views_reply(browser):
-    dropbox = dropbox_container.add_dropbox(message=u'Hello')
+def test_submitter_views_reply(testing, browser, dropbox):
     reply = u'Good bye und schöne Grüße.'
     author = u'John Doe'
     dropbox.add_reply(dict(reply=reply, author=author))
-    response = browser.get('/briefkasten/%s' % dropbox.drop_id)
+    response = browser.get(testing.route_url('dropbox_view', drop_id=dropbox.drop_id))
     response.mustcontain(reply)
     response.mustcontain(author)
