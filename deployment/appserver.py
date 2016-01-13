3f81104d071ec5072a0bf112668e845441e5d002
Tom Lazar <tomster@pyfidelity.com>
2016-01-13 17:16:21 +0100

    allow deploying development versions of the application
    we use a custom devpi instance to publish development versions with
    automatic version increments

--- deployment/appserver.py
+++ deployment/appserver.py
@@ -3,6 +3,7 @@ from os import path
 from fabric import api as fab
 from fabric.api import env, task
 from bsdploy.fabutils import rsync
+from ploy.config import value_asbool
 
 AV = None
 
@@ -53,3 +54,61 @@ def upload_pgp_keys():
             fab.sudo('''gpg --import %s/*.pgp''' % upload_target,
                 user=AV['appuser'], shell_escape=False)
         fab.run('rm -rf %s' % upload_target)
+
+
+@task
+def upload_backend(index='dev', user=None):
+    """
+    Build the backend and upload it to the remote server at the given index
+    """
+    get_vars()
+    login_devpi(user=user, index=index)
+    with fab.lcd('../application'):
+        fab.local('make upload')
+
+
+def briefkasten_ctl(action='restart'):
+    get_vars()
+    fab.sudo('supervisorctl {action} briefkasten'.format(
+        action=action,
+        **AV), warn_only=True)
+
+
+@task
+def update_backend(index='dev', build=False, user='briefkasten', version=None):
+    """
+    Install the backend from the given devpi index at the given version on the target host and restart the service.
+
+    If version is None, it defaults to the latest version
+
+    Optionally, build and upload the application first from local sources. This requires a
+    full backend development environment on the machine running this command (pyramid etc.)
+    """
+    get_vars()
+    if value_asbool(build):
+        upload_backend(index=index, user=user)
+    with fab.cd('{apphome}'.format(**AV)):
+        command = 'bin/pip install --upgrade --pre -i {ploy_default_publish_devpi}/{user}/{index}/+simple/ briefkasten'.format(
+            index=index,
+            user=user,
+            **AV)
+        if version:
+            command = '%s==%s' % (command, version)
+        fab.sudo(command)
+
+    briefkasten_ctl('restart')
+
+
+@task
+def login_devpi(index='dev', user=None):
+    get_vars()
+    if user is None:
+        user = fab.env['user']
+    publish_devpi = AV.get('ploy_default_publish_devpi')
+    fab.local('bin/devpi use {base_url}/briefkasten}/{index}'.format(
+        index=index,
+        base_url=publish_devpi,
+    ))
+    fab.local('bin/devpi login {user}'.format(user=user))
+
+
