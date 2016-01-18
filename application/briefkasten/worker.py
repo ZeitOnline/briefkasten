26a321146bb11eb46c8eaaf673137c29c9f62af9
Dirk Engling <erdgeist@erdgeist.org>
2016-01-18 16:32:45 +0100

    Combine directory event watcher and worker pools

--- application/briefkasten/worker.py
+++ application/briefkasten/worker.py
@@ -1,7 +1,10 @@
 import click
 from os import path
 from .dropbox import DropboxContainer
-
+from multiprocessing import Pool
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler, FileSystemEventHandler
+from threading import Condition
 
 def get_settings(fs_config):
     import ConfigParser
@@ -31,14 +34,43 @@ def get_settings(fs_config):
     required=False,
     default=None,
 )
-def main(config, drop_id=None):     # pragma: no cover
+
+class MyHandler(FileSystemEventHandler):
+    def __init__(self, main_loop_cond):
+        self.main_loop_cond = main_loop_cond
+
+    def on_modified(self, event):
+        self.main_loop_cond.acquire()
+        self.main_loop_cond.notify()
+        self.main_loop_cond.release()
+        print "directory modified"
+
+def process_drop(drop):
+    drop.process()
+    # todo: ensure it is in the right directory
+
+def main(config):     # pragma: no cover
     settings = get_settings(path.abspath(config))
     drop_root = DropboxContainer(settings=settings)
 
-    if drop_id is not None:
-        drops = [drop_root.get_dropbox(drop_id)]
-    else:
-        drops = drop_root
+    # Should point to the todo-directory, or drop dir
+    path = '.'
 
-    for drop in drops:
-        print(drop)
+    workers = Pool(processes=settings.workers)
+
+    condition = Condition()
+    event_handler = MyHandler(condition)
+
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=False)
+    observer.start()
+
+    condition.acquire()
+    while True:
+        for drop in drop_root:
+            print(drop)
+            if( drop.status_int == 200 ):
+                workers.map_async(process_drop, [drop])
+
+        condition.wait()
+    condition.release()
