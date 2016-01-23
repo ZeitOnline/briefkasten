627344c254ca6e55612f7b92da0bd3512afb7bdc
Tom Lazar <tomster@pyfidelity.com>
2016-01-23 02:39:47 +0100

    explicitly set timeout

--- application/briefkasten/notifications.py
+++ application/briefkasten/notifications.py
@@ -30,6 +30,7 @@ def setup_smtp_factory(**settings):
         port=int(settings.get('mail.port', 25)),
         user=settings.get('mail.user'),
         password=settings.get('mail.password'),
+        timeout=float(settings.get('mail.timeout', 60)),
     )
 
 
