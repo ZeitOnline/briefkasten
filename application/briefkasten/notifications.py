e3beb05d52b39ce55e1bc1730af10cd8a2102158
Tom Lazar <tomster@pyfidelity.com>
2016-01-23 02:31:58 +0100

    enforce int for port

--- application/briefkasten/notifications.py
+++ application/briefkasten/notifications.py
@@ -27,7 +27,7 @@ def setup_smtp_factory(**settings):
     """ expects a dictionary with 'mail.' keys to create an appropriate smtplib.SMTP instance"""
     return CustomSMTP(
         host=settings.get('mail.host', 'localhost'),
-        port=settings.get('mail.port', 25),
+        port=int(settings.get('mail.port', 25)),
         user=settings.get('mail.user'),
         password=settings.get('mail.password'),
     )
