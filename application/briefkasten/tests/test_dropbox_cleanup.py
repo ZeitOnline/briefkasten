df6a480809f83b095154c412d57064a9f31233e5
Tom Lazar <tomster@pyfidelity.com>
2016-01-27 13:24:41 +0100

    add more cleanup assertions

--- application/briefkasten/tests/test_dropbox_cleanup.py
+++ application/briefkasten/tests/test_dropbox_cleanup.py
@@ -1,20 +1,39 @@
-from os import listdir
+from os import listdir, mkdir
+from os.path import join
 from pytest import fixture
 
 
 @fixture
 def cleansed_dropbox(dropbox_container, dropbox):
-    """ TBD"""
-    pass
+    mkdir(join(dropbox.fs_path, 'clean'))
+    return dropbox
 
 
-def test_initial_backup(dropbox_container, dropbox):
+def test_cleanup_deletes_message(dropbox_container, dropbox):
+    assert 'message' in listdir(dropbox.fs_path)
+    dropbox.cleanup()
+    assert 'message' not in listdir(dropbox.fs_path)
+
+
+def test_cleanup_deletes_dirty_attachments(dropbox_container, dropbox):
+    assert 'attach' in listdir(dropbox.fs_path)
+    dropbox.cleanup()
+    assert 'attach' not in listdir(dropbox.fs_path)
+
+
+def test_cleanup_deletes_cleansed_attachments(dropbox_container, cleansed_dropbox):
+    assert 'clean' in listdir(cleansed_dropbox.fs_path)
+    cleansed_dropbox.cleanup()
+    assert 'clean' not in listdir(cleansed_dropbox.fs_path)
+
+
+def test_initial_backup_creation(dropbox_container, dropbox):
     assert 'dirty.zip.pgp' not in listdir(dropbox.fs_path)
     dropbox._create_backup()
     assert 'dirty.zip.pgp' in listdir(dropbox.fs_path)
 
 
-def test_initial_backup_removed_on_cleanup_after_success(dropbox_container, dropbox):
+def test_initial_backup_removed_on_cleanup(dropbox_container, dropbox):
     dropbox._create_backup()
     assert 'dirty.zip.pgp' in listdir(dropbox.fs_path)
     dropbox.cleanup()
