a5e91dc08c54b9869f518a338004923bb1ea07cb
Tom Lazar <tomster@pyfidelity.com>
2016-03-07 16:25:51 +0100

    sprinkle some comments

--- application/briefkasten/dropbox.py
+++ application/briefkasten/dropbox.py
@@ -253,6 +253,8 @@ class Dropbox(object):
     # "private" helper methods for processing a drop
 
     def _create_encrypted_zip(self, source='dirty', fs_target_dir=None):
+        """ creates a zip file from the drop and encrypts it to the editors.
+        the encrypted archive is created inside fs_target_dir"""
         backup_recipients = [r for r in self.editors if checkRecipient(self.gpg_context, r)]
 
         # this will be handled by watchdog, no need to send for each drop
@@ -260,6 +262,7 @@ class Dropbox(object):
             self.status = u'500 no valid keys at all'
             return self.status
 
+        # calculate paths
         fs_backup = join(self.fs_path, '%s.zip' % source)
         if fs_target_dir is None:
             fs_backup_pgp = join(self.fs_path, '%s.zip.pgp' % source)
@@ -269,12 +272,15 @@ class Dropbox(object):
             dirty=self.fs_dirty_attachments,
             clean=self.fs_cleansed_attachments
         )
+
+        # create archive
         with ZipFile(fs_backup, 'w', ZIP_STORED) as backup:
             if exists(join(self.fs_path, 'message')):
                 backup.write(join(self.fs_path, 'message'))
             for fs_attachment in fs_source[source]:
                 backup.write(fs_attachment)
 
+        # encrypt archive
         with open(fs_backup, "rb") as backup:
             self.gpg_context.encrypt_file(
                 backup,
@@ -283,6 +289,7 @@ class Dropbox(object):
                 output=fs_backup_pgp
             )
 
+        # cleanup
         remove(fs_backup)
         return fs_backup_pgp
 
