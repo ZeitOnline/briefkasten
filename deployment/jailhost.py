3208cb9403e2506094b9f95177348ba06a69e442
Tom Lazar <tomster@pyfidelity.com>
2016-03-11 17:17:15 +0100

    don't error if there is no authorized keys to delete during cleanup

--- deployment/jailhost.py
+++ deployment/jailhost.py
@@ -79,5 +79,5 @@ def reset_jails(confirm=True, keep_cleanser_master=True):
     with fab.warn_only():
         for jail in jails:
             fab.run('ezjail-admin delete -fw {jail}'.format(jail=jail))
-    # remove authorized keys for no longer existing key (they are regenerated for each new worker)
-    fab.run('rm /usr/jails/cleanser/usr/home/cleanser/.ssh/authorized_keys')
+        # remove authorized keys for no longer existing key (they are regenerated for each new worker)
+        fab.run('rm /usr/jails/cleanser/usr/home/cleanser/.ssh/authorized_keys')
