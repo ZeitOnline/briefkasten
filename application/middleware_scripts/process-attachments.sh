58ceb5937c1c5784b98de65901d864072aebfba2
Dirk Engling <erdgeist@erdgeist.org>
2016-06-09 11:29:52 +0200

    Report uncleansible files and cleansing errors

--- application/middleware_scripts/process-attachments.sh
+++ application/middleware_scripts/process-attachments.sh
@@ -29,23 +29,29 @@ process_single_file () {
 
   case ${the_type%;*} in
   text/plain)          cp                "${the_file}" "${the_destination}";;
-  text/html)           process-html.sh   "${the_file}" "${the_destination}";;
+#  text/html)           process-html.sh   "${the_file}" "${the_destination}";;
   application/msword)  process-msword.sh "${the_file}" "${the_destination}";;
   application/pdf)     process-pdf.sh    "${the_file}" "${the_destination}";;
-  audio/mpeg)          process-mpeg.sh   "${the_file}" "${the_destination}";;
+# audio/mpeg)          process-mpeg.sh   "${the_file}" "${the_destination}";;
   image/*)             process-image.sh  "${the_file}" "${the_destination}" "${the_type}";;
 
 # archive and compression format
-  application/zip)     process_zip.sh    "${the_file}" "${the_destination}";;
-  application/x-bzip2) process_bzip.sh   "${the_file}" "${the_destination}";;
-  application/x-tar)   process_tar.sh    "${the_file}" "${the_destination}";;
+# application/zip)     process-zip.sh    "${the_file}" "${the_destination}";;
+# application/x-bzip2) process-bzip.sh   "${the_file}" "${the_destination}";;
+# application/x-tar)   process-tar.sh    "${the_file}" "${the_destination}";;
 
 # every unknown format is just copied
 # this means that the server tried its best and is at least not worse
 # than plain email
-  *)                   cp                "${the_file}" "${the_destination}";;
+  *)                   cp                "${the_file}" "${the_destination}"
+                       printf "800 Not cleansible" > "${the_dropdir}"/status
+  ;;
 
   esac
+
+  [ $? -eq 0 ] && return 0
+  printf "540 Error while cleasing file of type %s" "${the_type%;*}" > "${the_dropdir}"/status
+  return 1
 }
 
 # define our bail out shortcut
@@ -146,9 +152,10 @@ fi
 # host, usually without the config parameter.
 printf "204 Attachments in quarantine on actual cleanser host.\n" > "${the_dropdir}"/status
 
-mkdir "${the_dropdir}"/clean
+mkdir -p "${the_dropdir}"/clean
 for the_attachment in "${the_dropdir}"/attach/*; do
-  [ -f "${the_attachment}" ] && process_single_file "${the_attachment}" ${the_dropdir}/clean
+  [ -f "${the_attachment}" ] || continue
+  process_single_file "${the_attachment}" ${the_dropdir}/clean || return 1
 done
 
 exit 0
