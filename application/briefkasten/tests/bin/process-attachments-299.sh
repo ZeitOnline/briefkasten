41ea36173e83e3af9ff06fca360f657c4f502056
Tom Lazar <tomster@pyfidelity.com>
2016-06-13 15:25:50 +0200

    let the mocked process-attachments script be more flexible
    it now can have custome behaviour depending on the desired status we set
    via environment variable
    
    this will allow us to test edgecases

--- application/briefkasten/tests/bin/process-attachments-299.sh
+++ application/briefkasten/tests/bin/process-attachments-299.sh
@@ -10,6 +10,8 @@
 exerr () { echo "ERROR: $*" >&2 ; exit 1; }
 exnerr() { printf "%s %s\n" $1 $2 > "${the_dropdir}"/status; exit 1; }
 
+: ${MOCKED_STATUS_CODE:=299}
+
 # this is the usage string in case of error
 usage="process-attachments.sh [-d dropdir] [-c config]"
 
@@ -26,7 +28,27 @@ esac; done; shift $(( ${OPTIND} - 1 ))
 # If it is not there, we got nothing to do
 [ -d "${the_dropdir}"/attach/ ] || exit 0
 
-mv "${the_dropdir}"/attach "${the_dropdir}"/clean
-mkdir "${the_dropdir}"/attach
-printf "299 Cleansed" > "${the_dropdir}"/status
+case ${MOCKED_STATUS_CODE} in
+    299)
+        mv "${the_dropdir}"/attach "${the_dropdir}"/clean
+        mkdir "${the_dropdir}"/attach
+        printf "299 Cleansed" > "${the_dropdir}"/status
+        exit 0
+        ;;
+    800)
+        mv "${the_dropdir}"/attach "${the_dropdir}"/clean
+        mkdir "${the_dropdir}"/attach
+        printf "800 At least one uncleansed" > "${the_dropdir}"/status
+        exit 0
+        ;;
+    540)
+        printf "540 Error while cleaning" > "${the_dropdir}"/status
+        exit 1
+        ;;
+    *)
+        printf "%s Generic mocked code" ${MOCKED_STATUS_CODE} > "${the_dropdir}"/status
+        exit 1
+        ;;
+esac
+
 exit 0
