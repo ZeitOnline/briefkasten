fb59298d597f4ca141f6291320db8ffb3a72f7c5
Tom Lazar <tomster@pyfidelity.com>
2016-09-01 17:09:18 +0200

    allow updating from pypi, too

--- deployment/appserver.py
+++ deployment/appserver.py
@@ -73,7 +73,7 @@ def briefkasten_ctl(action='restart'):
 
 
 @task
-def update_backend(index='dev', build=True, user=None, version=None):
+def update_backend(use_pypi=False, index='dev', build=True, user=None, version=None):
     """
     Install the backend from the given devpi index at the given version on the target host and restart the service.
 
@@ -86,10 +86,13 @@ def update_backend(index='dev', build=True, user=None, version=None):
     if value_asbool(build):
         upload_backend(index=index, user=user)
     with fab.cd('{apphome}'.format(**AV)):
-        command = 'bin/pip install --upgrade --pre -i {ploy_default_publish_devpi}/briefkasten/{index}/+simple/ briefkasten'.format(
-            index=index,
-            user=user,
-            **AV)
+        if value_asbool(use_pypi):
+            command = 'bin/pip install --upgrade briefkasten'
+        else:
+            command = 'bin/pip install --upgrade --pre -i {ploy_default_publish_devpi}/briefkasten/{index}/+simple/ briefkasten'.format(
+                index=index,
+                user=user,
+                **AV)
         if version:
             command = '%s==%s' % (command, version)
         fab.sudo(command)
