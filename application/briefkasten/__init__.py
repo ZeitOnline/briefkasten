24e603fa41ed30276e034f117b8226462b7fe004
Tom Lazar <tomster@pyfidelity.com>
2018-01-19 14:48:29 +0100

    Make the submission form timeout configurable

--- application/briefkasten/__init__.py
+++ application/briefkasten/__init__.py
@@ -21,9 +21,15 @@ def dropbox_post_factory(request):
     """receives a UUID via the request and returns either a fresh or an existing dropbox
     for it"""
     try:
+        max_age = int(request.registry.settings.get('post_token_max_age_seconds'))
+    except Exception:
+        max_age = 300
+
+    try:
         drop_id = parse_post_token(
             token=request.matchdict['token'],
-            secret=request.registry.settings['post_secret'])
+            secret=request.registry.settings['post_secret'],
+            max_age=max_age)
     except SignatureExpired:
         raise HTTPGone('dropbox expired')
     except Exception:  # don't be too specific on the reason for the error
