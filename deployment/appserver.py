499f2cf582b961c276fd6d34cf31c42e700463b1
Tom Lazar <tomster@pyfidelity.com>
2018-04-24 13:41:18 +0200

    Fix paths for updating theme via fabfile
    (The playbook already did the right thing)

--- deployment/appserver.py
+++ deployment/appserver.py
@@ -25,9 +25,17 @@ def get_vars():
 def upload_theme():
     """ upload and/or update the theme with the current git state"""
     get_vars()
-    with fab.settings(fab.hide('running')):
-        local_theme_path = path.abspath(path.join(fab.env['config_base'], fab.env.instance.config['local_theme_path']))
-        rsync('-av', '--delete', local_theme_path, '{host_string}:%s/' % AV['themes_dir'])
+    with fab.settings():
+        local_theme_path = path.abspath(
+            path.join(fab.env['config_base'],
+                fab.env.instance.config['local_theme_path']))
+        rsync(
+            '-av',
+            '--delete',
+            '%s/' % local_theme_path,
+            '{{host_string}}:{themes_dir}/{ploy_theme_name}'.format(**AV)
+        )
+        briefkasten_ctl('restart')
 
 
 @task
