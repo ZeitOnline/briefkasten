1798319a07bc1f159a8d904d7888637fb09249c4
Tom Lazar <tom.lazar@zeit.de>
2019-05-27 12:55:32 +0200

    FIX: include drop_id so it can be rendered on the response form

--- application/briefkasten/views.py
+++ application/briefkasten/views.py
@@ -155,6 +155,7 @@ def dropbox_reply_submitted(dropbox, request):
             title='%s - %s' % (title, dropbox.status),
             action=request.url,
             message=u'Alle Felder müssen ausgefüllt werden.',
+            drop_id=dropbox.drop_id,
         )
         return appstruct
     dropbox.add_reply(data)
