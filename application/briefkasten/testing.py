bca71cbe6f068ce027653b6fb6a3aafa2b669f84
Tom Lazar <tom.lazar@zeit.de>
2020-05-12 16:44:36 +0200

    Py3: use file system based loader
    using a package loader for test assets was misguided to begin with since
    tests are not part of the package and not importable.
    
    that it worked under python 2 (and or with an older version of jinja) is
    just a coincidence)

--- application/briefkasten/testing.py
+++ application/briefkasten/testing.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import shutil
 from cgi import FieldStorage
-from jinja2 import Environment, PackageLoader
+from jinja2 import Environment, FileSystemLoader
 from os.path import abspath, dirname, join
 from mock import Mock
 from pyramid.testing import DummyRequest, setUp, tearDown
@@ -10,11 +10,12 @@ from urllib.parse import unquote
 from webtest import TestApp
 
 
-jinja_env = Environment(loader=PackageLoader('briefkasten', 'tests'))
-
-
 def asset_path(*parts):
     return abspath(join(dirname(__file__), 'tests', *parts))
+
+
+jinja_env = Environment(
+    loader=FileSystemLoader(asset_path()))
 
 
 @fixture(scope="function")
