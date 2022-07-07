8fa832293b2f447b1e489903b6e529ca7da8b141
Andreas Zeidler <andreas.zeidler@zeit.de>
2022-07-07 10:30:45 +0200

    ZO-1506: Don't copy all the deployment data into the image, but mount it
    A simple shell script is provided for convenience so we can run:
    
      $ cd deployment/
      $ ./ploy.sh
    
    And then inside the docker container:
    
      root@a6839f74f62b:/briefkasten# make
    
    Note that for this to work the `briefkasten` checkout needs to
    be located "inside" the `briefkasten-config` checkout using the
    recently introduced git submodule setup, see
    https://github.com/ZeitOnline/briefkasten-config/commit/05e9da7c.

---
+++ deployment/ploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# note that for this to work the `briefkasten` checkout needs to be located
+# "inside" the `briefkasten-config` checkout using the submodule setup as
+# added in https://github.com/ZeitOnline/briefkasten-config/commit/05e9da7c
+
+docker build -t briefkasten-ploy .
+docker run -it \
+	-v $PWD:/briefkasten \
+	-v $PWD/../../etc:/briefkasten/etc \
+	briefkasten-ploy
